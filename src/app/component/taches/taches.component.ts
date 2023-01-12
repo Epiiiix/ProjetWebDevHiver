c613e8b219e27ff19ab6ae46fb5d4159a15a2f61
Epiiiix <luigi.giusiano@gmail.com>
2023-01-12 16:35:50 +0100

    Update

--- src/app/component/taches/taches.component.ts
+++ src/app/component/taches/taches.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Tache } from 'src/app/model/tache';
 import { TachesService } from 'src/app/service/taches.service';
 import { UserService } from 'src/app/service/user.service';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-taches',
@@ -56,87 +56,90 @@ export class TachesComponent implements OnInit {
     });
   }
 
-  ajouterUndefined() {
-    this.tacheService.ajoutTaches(this.newTacheUndefined).subscribe({
-      next: (data) => {
-        this.Undefined.push(data);
-      }
-    });
-  }
-
-  ajouterEnAttente() {
-    this.tacheService.ajoutTaches(this.newTacheEnAttente).subscribe({
-      next: (data) => {
-        this.EnAttente.push(data);
-      }
-    });
-  }
-
-  ajouterEnCours() {
-    this.tacheService.ajoutTaches(this.newTacheEnCours).subscribe({
-      next: (data) => {
-        this.EnCours.push(data);
-      }
-    });
-  }
-
-  ajouterTermine() {
-    this.tacheService.ajoutTaches(this.newTacheTermine).subscribe({
-      next: (data) => {
-        this.Termine.push(data);
-      }
-    });
-  }
 
 
-supprimer(tache: Tache): void {
-  this.tacheService.removeTaches(tache).subscribe({
-    next: (data) => {
-      switch (tache.statut) {
-        case 'Undefined':
-          this.Undefined = this.Undefined.filter(t => tache._id !== t._id);
-          break;
-        case 'En Attente':
-          this.EnAttente = this.EnAttente.filter(t => tache._id !== t._id);
-          break;
-        case 'En Cours':
-          this.EnCours = this.EnCours.filter(t => tache._id !== t._id);
-          break;
-        case 'Termine':
-          this.Termine = this.Termine.filter(t => tache._id !== t._id);
-          break;
-      }
+  ajouter(statut: string) {
+    switch (statut) {
+      case 'Undefined':
+        this.tacheService.ajoutTaches(this.newTacheUndefined).subscribe({
+          next: (data) => {
+            this.Undefined.push(data);
+          }
+        });
+        break;
+      case 'En Attente':
+        this.tacheService.ajoutTaches(this.newTacheEnAttente).subscribe({
+          next: (data) => {
+            this.EnAttente.push(data);
+          }
+        });
+        break;
+      case 'En Cours':
+        this.tacheService.ajoutTaches(this.newTacheEnCours).subscribe({
+          next: (data) => {
+            this.EnCours.push(data);
+          }
+        });
+        break;
+      case 'Termine':
+        this.tacheService.ajoutTaches(this.newTacheTermine).subscribe({
+          next: (data) => {
+            this.Termine.push(data);
+          }
+        });
+        break;
     }
-  });
-}
-
-modifier(tache: Tache) {
-  tache.termine = !tache.termine;
-  this.tacheService.updateTaches(tache).subscribe({
-    next: (data) => {
-    }
-  });
-}
-
-drop(event: CdkDragDrop<Tache[]>) {
-  if (event.previousContainer === event.container) {
-    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-  } else {
-    transferArrayItem(event.previousContainer.data,
-                      event.container.data,
-                      event.previousIndex,
-                      event.currentIndex);
   }
-}
-loggout() {
-  this.userService.logout().subscribe(() => {
-    this.router.navigate(['']);
-  })
-}
 
-change(filter: string) {
-  this.filter = filter;
-}
+  supprimer(tache: Tache): void {
+    this.tacheService.removeTaches(tache).subscribe({
+      next: (data) => {
+        switch (tache.statut) {
+          case 'Undefined':
+            this.Undefined = this.Undefined.filter(t => tache._id !== t._id);
+            break;
+          case 'En Attente':
+            this.EnAttente = this.EnAttente.filter(t => tache._id !== t._id);
+            break;
+          case 'En Cours':
+            this.EnCours = this.EnCours.filter(t => tache._id !== t._id);
+            break;
+          case 'Termine':
+            this.Termine = this.Termine.filter(t => tache._id !== t._id);
+            break;
+        }
+      }
+    });
+  }
 
+  modifier(tache: Tache) {
+    tache.termine = !tache.termine;
+    this.tacheService.updateTaches(tache).subscribe({
+      next: (data) => {
+      }
+    });
+  }
 
+  drop(event: CdkDragDrop<Tache[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+      const tache = event.container.data[event.currentIndex];
+      tache.statut = event.container.id;
+      this.tacheService.updateTaches(tache).subscribe();
+    }
+  }
+  loggout() {
+    this.userService.logout().subscribe(() => {
+      this.router.navigate(['']);
+    })
+  }
+
+  change(filter: string) {
+    this.filter = filter;
+  }
 }
